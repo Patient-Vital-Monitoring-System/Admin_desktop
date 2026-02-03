9c8b30d8c650c85491bba3a17f3869bab6e3023e
xhinsade29 <lopezkristine749@gmail.com>
2026-02-03 22:05:32 +0800

    added the dependencies like script and stylesheets

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-3
+const burgerBtn = document.getElementById('burgerBtn');
+        const sidebar = document.getElementById('sidebar');
+        
+        burgerBtn.addEventListener('click', () => {
+            burgerBtn.classList.toggle('active');
+            sidebar.classList.toggle('active');
+        });
+        
+        sidebar.addEventListener('click', (e) => {
+            if (e.target.tagName === 'A') {
+                burgerBtn.classList.remove('active');
+                sidebar.classList.remove('active');
+            }
+        });
